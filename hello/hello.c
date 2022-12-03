e77543fb58f65d045b9bfab2e479fdaa418c93c8
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-03 20:47:37 +0000

    Sat, Dec 3, 2022, 8:47 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void){
-    printf("Hello, world\n");
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    printf("Hello, %s\n", name);
 }
