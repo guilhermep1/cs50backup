5e98cea587ea403172bc38acd9b44beaac9968a8
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-04 10:25:52 +0000

    Sun, Dec 4, 2022, 10:25 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,7 +6,7 @@ long checksum(long credit);
 int main(void)
 {
     long credit = get_long("Number: ");
-    printf("%ld\n", credit%100);
+    printf("%ld\n", credit/10);
 }
 
 // long checksum(credit)
