edb603960a7cf7585cef7726299a467b7410f0fc
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-04 10:34:59 +0000

    Sun, Dec 4, 2022, 10:34 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,10 +9,11 @@ int main(void)
     checksum(credit);
 }
 
-checksum(credit)
+long checksum(long credit)
 {
     while(credit>=10){
         credit = credit/10;
-        printf(credit%10);
+        printf("%ld",credit%10);
     }
+    return 0;
 }
