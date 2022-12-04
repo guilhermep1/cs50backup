38f45943c5b33ee866895aa0656be50478d1a4d1
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-04 10:20:21 +0000

    Sun, Dec 4, 2022, 10:20 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,14 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
-long checksum(credit);
+long checksum(long credit);
 
 int main(void)
 {
     long credit = get_long("Number: ");
+    printf("%ld\n", credit%15);
 }
 
-long checksum(credit)
-{
-    
-}
+// long checksum(credit)
+// {
+
+// }
