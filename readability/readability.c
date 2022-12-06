ecf34b2e5e35ee86a07a4687db18e462026e128e
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-06 15:58:32 +0000

    Tue, Dec 6, 2022, 3:58 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,6 +1,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int count_letters(string text);
 int count_words(string text);
@@ -8,7 +9,12 @@ int count_sentences(string text);
 
 int main(void)
 {
-    string text = get_string("Text: ");
+    string text;
+    do
+    {
+        text = get_string("Text: ");
+    }
+    while(strlen(text) <= 0);
     printf("%d letters\n", count_letters(text));
     printf("%d words\n", count_words(text));
     printf("%d sentences\n", count_sentences(text));
