eaecd92454a64b924646b0db1c0a6d78d5ad8dc0
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-06 12:52:22 +0000

    Tue, Dec 6, 2022, 12:52 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 #include <string.h>
 
+int count_letters(string text);
+
 int main(void)
 {
     string text = get_string("Text: ");
+    printf("%d\n",count_)
 }
 
 int count_letters(string text)
 {
-    int letters = 0;
+    int letters = 0, i = 0;
+    while(text[i] != "\0")
+    {
+        letters++;
+        i++;
+    }
+    return letters;
 }
