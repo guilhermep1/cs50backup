e0e1f235fd93988b8edea1e7f64832a40ea0e420
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-07 11:40:18 +0000

    Wed, Dec 7, 2022, 11:40 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -16,7 +16,7 @@ int main(void)
 
     do
     {
-        text = "get_string("Text: ")";
+        text = "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard.";
     }
     while (strlen(text) <= 0);
 
