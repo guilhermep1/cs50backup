3b390b8863c7f846afdb647f4d0063c749dd773a
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-08 18:02:00 +0000

    Thu, Dec 8, 2022, 6:02 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,11 +6,10 @@
 
 int main(int argc, string argv[])
 {
-    printf("%d\n%s\n", argc, argv[1]);
     //passing argument as variable
     int key = atoi(argv[1]);
     //Making sure user input is correct
-    if(argc == 2 && isalnum(key) && key > 0)
+    if(argc == 2 && key > 0)
     {
         //Execute normally
         printf("key: %d\n", key);
