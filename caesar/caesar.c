b50181423f1a61169d800839b71512c974961594
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-08 17:42:36 +0000

    Thu, Dec 8, 2022, 5:42 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -4,9 +4,9 @@
 int main(int argc, string argv[])
 {
     //Making sure user input is correct
-    if(argc == 1)
+    if(argc == 1 && argv[0] )
     {
         //Execute normally
-        
+
     }
 }
