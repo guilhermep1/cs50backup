c8b6b1c702fc58ebc040e1aafe0f656ca5fd994b
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-08 18:03:13 +0000

    Thu, Dec 8, 2022, 6:03 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,7 +1,6 @@
 #include <cs50.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <string.h>
 
 int main(int argc, string argv[])
@@ -12,7 +11,8 @@ int main(int argc, string argv[])
     if(argc == 2 && key > 0)
     {
         //Execute normally
-        printf("key: %d\n", key);
+        //Encryption algorithm
+        
         return 0;
     }
     else
