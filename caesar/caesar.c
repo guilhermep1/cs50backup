bb0742da05b64c0e9a1dfeda048d04c4d9f7c9d3
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-08 17:51:17 +0000

    Thu, Dec 8, 2022, 5:51 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -1,11 +1,14 @@
 #include <cs50.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <ctype.h>
 
 int main(int argc, string argv[])
 {
+    //passing argument as variable
+    int key = atoi(argv[1]);
     //Making sure user input is correct
-    if(argc == 1 && isalnum(argv[0]) && argv[0] > 0)
+    if(argc == 1 && isalnum(key) && argv[0] > 0)
     {
         //Execute normally
         return 0;
