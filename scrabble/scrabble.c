57532f8dee1a9db80f51555d7ccca5d130e98f72
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-12 19:25:17 +0000

    Mon, Dec 12, 2022, 7:25 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -19,9 +19,18 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
+    if(score1 > score2)
+    {
+        printf("%d\n", score1);
+    }
+    else
+    {
+        printf("%d\n", score2);
+    }
 }
 
 int compute_score(string word)
 {
     // TODO: Compute and return score for string
+    
 }
