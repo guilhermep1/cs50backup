828dcdd73331e4bd9b523bb227d154a3fd24aa40
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-14 15:00:48 +0000

    Wed, Dec 14, 2022, 3:00 PM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -68,8 +68,7 @@ bool vote(string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        printf("%s = %s\n", candidates[i].name, name);
-        if(candidates[i].name == name)
+        if(strcmp(candidates[i].name, name) == 0)
         {
             candidates[i].votes += 1;
             return true;
