a8dda2c9a37288e359915783da2ba8a8a9ec10fd
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-15 15:04:01 +0000

    Thu, Dec 15, 2022, 3:04 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -146,9 +146,9 @@ void tabulate(void)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if(candidates[j].eliminated == false && strcmp(candidates[j].name, ))
+            if(candidates[j].eliminated == false && strcmp(candidates[j].name, preferences[i][j]) == 0)
             {
-
+                candidates[j].votes++;
             }
         }
     }
