45514cfa5d9cad93420ad568552b075d53a65816
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-15 14:25:25 +0000

    Thu, Dec 15, 2022, 2:25 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max voters and candidates
 #define MAX_VOTERS 100
@@ -127,7 +128,10 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(name, ))
+    }
     return false;
 }
 
