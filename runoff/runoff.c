d33320f01e54c69e1e2ab1c914a3908be219da6a
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-15 14:26:51 +0000

    Thu, Dec 15, 2022, 2:26 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -130,7 +130,11 @@ bool vote(int voter, int rank, string name)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        if (strcmp(name, ))
+        if (strcmp(name, candidate[i].name) == 0)
+        {
+            candidate[i].votes++;
+            return true;
+        }
     }
     return false;
 }
