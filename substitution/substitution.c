08ee66d1c5ee968f91f91a1db475953c2c71a2e5
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-20 19:47:55 +0000

    Tue, Dec 20, 2022, 7:47 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -18,6 +18,7 @@ int main(int argc, string argv[])
         else
         {
             string plaintext = get_string("plaintext: ");
+            printf("ciphertext: %s\n", encrypt(plaintext, key));
             return 0;
         }
     }
