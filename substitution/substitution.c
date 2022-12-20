6d4333474d274a4fd1bd96f9660e8fc89282c68d
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-20 19:29:22 +0000

    Tue, Dec 20, 2022, 7:29 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,15 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+string encrypt(string plaintext, string key);
+
 int main(int argc, string argv[])
 {
     if (argc == 2)
     {
-        
+        string key = argv[1];
+
     }
     else
     {
         printf("Usage: ./substitution key\n");
         return 1;
     }
+}
+
+string encrypt(string plaintext, string key)
+{
+
 }
