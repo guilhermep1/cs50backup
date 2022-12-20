3d85e54fb0cb9c38e3a381584b8aedda9baff1cb
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-20 19:41:39 +0000

    Tue, Dec 20, 2022, 7:41 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -7,7 +7,7 @@ string encrypt(string plaintext, string key);
 
 int main(int argc, string argv[])
 {
-    if (argc == 2 && ispalha(argv[1]))
+    if (argc == 2)
     {
         string key = argv[1];
         if(strlen(key) != 26)
@@ -17,7 +17,7 @@ int main(int argc, string argv[])
         }
         else
         {
-            
+
         }
     }
     else
