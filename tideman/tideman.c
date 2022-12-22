e289578d8ac03ada8101ad8ef61a2327626fb787
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-22 16:35:28 +0000

    Thu, Dec 22, 2022, 4:35 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -100,7 +100,10 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
-    if(name)
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return false;
 }
 
