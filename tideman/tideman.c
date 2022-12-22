9524a29134c214bc156657fb75e15704e3a8b47d
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-22 17:01:48 +0000

    Thu, Dec 22, 2022, 5:01 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -120,7 +120,7 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    
+    for (int i = 0; i < candidate_count; )
     return;
 }
 
