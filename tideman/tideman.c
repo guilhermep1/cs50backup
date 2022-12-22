50e843052a360714e87d71579cb238e87baae786
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-22 17:36:49 +0000

    Thu, Dec 22, 2022, 5:36 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -124,7 +124,7 @@ void record_preferences(int ranks[])
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            preferences[i][j] += 
+            if()
         }
     }
     return;
