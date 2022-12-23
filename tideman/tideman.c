8a402fff5ee64020dbc16475ec4cd3b4d47c5edd
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-23 22:29:38 +0000

    Fri, Dec 23, 2022, 10:29 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -146,7 +146,7 @@ void add_pairs(void)
                 pairs[pair_count].winner = i;
                 pairs[pair_count].loser = j;
                 pair_count++;
-                printf("pair %i: winner: %i loser: %i\n", i, pairs[i].winner, pairs[i].loser);
+                printf("pair %i: winner: %i loser: %i\n", pair_count, pairs[i].winner, pairs[i].loser);
                 printf("pair count: %i\n", pair_count);
             }
         }
