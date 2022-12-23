cee12d06c6bb8144079c9edd367387ad0f65a094
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-23 15:39:15 +0000

    Fri, Dec 23, 2022, 3:39 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -136,7 +136,10 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return;
 }
 
