6826fa02f48d4ecd2df375a2de9eb8cc004cd1be
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-25 16:00:44 +0000

    Sun, Dec 25, 2022, 4:00 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -194,9 +194,15 @@ void lock_pairs(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for(int j = 0; j < pair_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
-            if()
+            for (int k = 0; k < pair_count; k++)
+            {
+                if (pairs[k].winner == i && pairs[k].loser == j)
+                {
+                    locked[i][j] = true;
+                }
+            }
         }
     }
     return;
