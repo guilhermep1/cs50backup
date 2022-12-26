ec30d05219f4a7ffbdb7f74aca5c2bc94bf420c2
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-26 19:18:06 +0000

    Mon, Dec 26, 2022, 7:18 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -174,7 +174,8 @@ void sort_pairs(void)
         int strongest = i;
         for(int j = 0; j < pair_count; j++)
         {
-            if (preferences[pairs[i].winner][pairs[i].loser] < preferences[pairs[j].winner][pairs[j].loser])
+            if (preferences[pairs[i].winner][pairs[i].loser] - preferences[pairs[i].loser][pairs[i].winner]
+            < preferences[pairs[j].winner][pairs[j].loser] - preferences[pairs[j].loser][pairs[j].winner])
             {
                 strongest = j;
             }
