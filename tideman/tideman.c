74e8ee6e10341efc112da67f0ec361a8ff983497
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-27 00:10:29 +0000

    Tue, Dec 27, 2022, 12:10 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -225,7 +225,14 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (locked[j][i] == true)
+            {
+                break;
+            }
+            source = i
+        }
     }
     return;
 }
