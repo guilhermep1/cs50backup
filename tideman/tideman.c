b96937b83501e99dccdcfe1e2399209870d758d4
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2022-12-27 16:16:48 +0000

    Tue, Dec 27, 2022, 4:16 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -202,11 +202,11 @@ void lock_pairs(void)
 //Checks if locking a pair creates a cycle
 bool check_cycle(int pairindex)
 {
-    for (int i = 0; i < candidate_count; i++)
+    for (int i = pairs[pairindex]; i < candidate_count; i++)
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            
+
         }
     }
 
