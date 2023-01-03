c34b3fc94fbad66f69f5a84589d569e591a9de98
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2023-01-03 12:54:44 +0000

    Tue, Jan 3, 2023, 12:54 PM +00:00

--- test.c
+++ test.c
@@ -1,13 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void sortarr(void);
+
 int main(void)
 {
+    //defining array length and values
     int arr[10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    //sorting array
     sortarr();
+    //printing sorted array
     printf("Sorted array: \n");
     for (int i = 0; i < 10; i++)
     {
-        
+        printf("%i ", arr[i]);
     }
+}
+
+void sortarr(void)
+{
+    
 }
