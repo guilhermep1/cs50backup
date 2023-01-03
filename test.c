b44002ccc95b4b1fb30124f4ecd0c0e0af950dbe
Guilherme Plácido <62665317+guilhermep1@users.noreply.github.com>
2023-01-03 12:51:21 +0000

    Tue, Jan 3, 2023, 12:51 PM +00:00

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[10] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    sortarr();
+    printf("Sorted array: \n");
+    for (int i = 0; i < 10; i++)
+    {
+        
+    }
+}
